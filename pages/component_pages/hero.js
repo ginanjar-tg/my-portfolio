507afec17322003e985c4d5f51e9ffbaaf116b97
ginanjartg <ginanjar.10119032@mahasiswa.unikom.ac.id>
2022-07-05 17:05:04 +0700

    Update hero.js

--- pages/component_pages/hero.js
+++ pages/component_pages/hero.js
@@ -49,7 +49,7 @@ export default function Hero() {
               I fell in love with programming and I have at least learnt
               something, I think.. <br />
               I am fluent in classics like Java and MySQL. <br />
-              My field of Interest's are building new Web Technologies and
+              My field of Interests are building new Web Technologies and
               Products. <br />
               Whenever possible, I also apply my passion for developing products
               <br />
