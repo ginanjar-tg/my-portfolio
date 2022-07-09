395b794849a1e3d436113f33b9a51946ea390a44
ginanjar-tg <63410140+ginanjar-tg@users.noreply.github.com>
2022-07-09 15:21:14 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Navbar from "./component_pages/navbar.js";
 export default function Home() {
   return (
     <>
+    <head>
+  <title>GinanjarTG</title>
+</head>
       <div className="bg-gray-800">
         <Navbar />
       </div>
