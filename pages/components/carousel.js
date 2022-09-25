eafdd28cba4010342c792b39fe8a927f851abb22
ginanjartg <ginanjar0822@gmail.com>
2022-09-25 15:13:45 +0700

    fix carousel
    change the picture later.....

--- pages/components/carousel.js
+++ pages/components/carousel.js
@@ -1,7 +1,8 @@
 // this carousel will be used in contents.js
 import React from "react";
 import Bahan from "../../public/projects/bahan/bahan.png";
-import ImageZoom from "react-image-zooom";
+import Image from "next/image";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const Carousel = () => {
   return (
@@ -11,9 +12,9 @@ const Carousel = () => {
       data-bs-ride="carousel"
     >
       <div className="w-full flex">
-        <div className="carousel-inner relative w-1/2 overflow-hidden tex-center m-auto">
+        <div className="carousel-inner relative w-full md:w-1/2 overflow-hidden tex-center m-auto">
           <div className="carousel-item active relative float-left w-full">
-            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900">
+            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900 text-center">
               <p className="p-5">
                 <div
                   id="item1"
@@ -26,43 +27,128 @@ const Carousel = () => {
                 with a login system.
               </p>
               <div className="relative mx-auto p-5">
-                <ImageZoom src={Bahan} alt="CRUD Bahan Baku Bakso" zoom="200" />
+                <TransformWrapper initialScale={1}>
+                  {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                    <React.Fragment>
+                      <TransformComponent>
+                        <Image src={Bahan} className="rounded-lg" />
+                      </TransformComponent>
+
+                      <div className="tools space-x-3 text-center py-3 text-sm">
+                        <button
+                          onClick={() => zoomIn()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom In
+                        </button>
+                        <button
+                          onClick={() => zoomOut()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom Out
+                        </button>
+                        <button
+                          onClick={() => resetTransform()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Reset
+                        </button>
+                      </div>
+                    </React.Fragment>
+                  )}
+                </TransformWrapper>
               </div>
             </div>
           </div>
           <div className="carousel-item relative float-left w-full">
-            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900">
+            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900 text-center">
               <p className="p-5">
                 <div
                   id="item1"
                   className="text-xl font-bold mb-5 md:mt-0 mt-5 text-center"
                 >
-                  Sistem Manajemen Alamat
+                  3D Landing Page of Meatball Restaurant
                 </div>
-                A CRUD system to save the data of meatball ingredients stock.
-                This app using CodeIgniter framework and also has been equipped
-                with a login system.
+                Made with Three.js, this landing page is a 3D restaurant website
               </p>
               <div className="relative mx-auto p-5">
-                <ImageZoom src={Bahan} alt="CRUD Bahan Baku Bakso" zoom="200" />
+                <TransformWrapper initialScale={1}>
+                  {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                    <React.Fragment>
+                      <TransformComponent>
+                        <Image src={Bahan} className="rounded-lg" />
+                      </TransformComponent>
+
+                      <div className="tools space-x-3 text-center py-3 text-sm">
+                        <button
+                          onClick={() => zoomIn()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom In
+                        </button>
+                        <button
+                          onClick={() => zoomOut()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom Out
+                        </button>
+                        <button
+                          onClick={() => resetTransform()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Reset
+                        </button>
+                      </div>
+                    </React.Fragment>
+                  )}
+                </TransformWrapper>
               </div>
             </div>
           </div>
           <div className="carousel-item relative float-left w-full">
-            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900">
+            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900 text-center">
               <p className="p-5">
                 <div
                   id="item1"
                   className="text-xl font-bold mb-5 md:mt-0 mt-5 text-center"
                 >
-                  Sistem Manajemen Kucing
+                  Sistem Manajemen Lokasi By Maps
                 </div>
-                A CRUD system to save the data of meatball ingredients stock.
-                This app using CodeIgniter framework and also has been equipped
-                with a login system.
+                Made with React.js, this app is a location management system
+                that uses Mapbox API to display the location. This app also has
+                a feature to detect weather.
               </p>
               <div className="relative mx-auto p-5">
-                <ImageZoom src={Bahan} alt="CRUD Bahan Baku Bakso" zoom="200" />
+                <TransformWrapper initialScale={1}>
+                  {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                    <React.Fragment>
+                      <TransformComponent>
+                        <Image src={Bahan} className="rounded-lg" />
+                      </TransformComponent>
+
+                      <div className="tools space-x-3 text-center py-3 text-sm">
+                        <button
+                          onClick={() => zoomIn()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom In
+                        </button>
+                        <button
+                          onClick={() => zoomOut()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom Out
+                        </button>
+                        <button
+                          onClick={() => resetTransform()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Reset
+                        </button>
+                      </div>
+                    </React.Fragment>
+                  )}
+                </TransformWrapper>
               </div>
             </div>
           </div>
