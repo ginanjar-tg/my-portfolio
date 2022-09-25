f37891d75614101f7423d28abe18d9363bad1f5a
ginanjar-tg <63410140+ginanjar-tg@users.noreply.github.com>
2022-09-25 08:59:49 +0700

    Modified _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
+
+
+useEffect(() => {
+const use = async () => {
+  (await import('tw-elements')).default;
+    };
+    use();
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
