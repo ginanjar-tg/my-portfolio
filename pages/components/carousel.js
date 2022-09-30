a6cf24061b1a085fcaa7d91a842cc63fd27260b3
ginanjartg <ginanjar0822@gmail.com>
2022-09-30 10:09:28 +0700

    add some projects collection

--- pages/components/carousel.js
+++ pages/components/carousel.js
@@ -1,6 +1,8 @@
 // this carousel will be used in contents.js
 import React from "react";
 import Bahan from "../../public/projects/bahan/bahan.png";
+import Location from "../../public/projects/location/location.png";
+import Isekai from "../../public/projects/isekai/isekaistore.png";
 import Image from "next/image";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
@@ -10,7 +12,7 @@ const Carousel = () => {
       id="carouselExampleControls"
       className="carousel slide relative"
       data-bs-ride="carousel"
-data-bs-interval="false"
+      data-bs-interval="false"
     >
       <div className="w-full flex">
         <div className="carousel-inner relative w-full md:w-1/2 overflow-hidden tex-center m-auto">
@@ -21,103 +23,122 @@ data-bs-interval="false"
                   id="item1"
                   className="text-xl font-bold mb-5 md:mt-0 mt-5 text-center"
                 >
-                  Sistem Manajemen Bahan Baku Bakso
+                  Isekai Store Landing Page
+                </div>
+                Made with NextJS, this website is an official landing page of
+                Isekai Store, which is a web marketplace that sells stuff from
+                other world, or known as Isekai. You can check the website{" "}
+                <a
+                  href="https://isekaistore.vercel.app/"
+                  target="_blank"
+                  className="text-blue-500"
+                >
+                  here
+                </a>
+                .
+              </p>
+              <div className="relative mx-auto p-5">
+                <TransformWrapper initialScale={1}>
+                  {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                    <React.Fragment>
+                      <TransformComponent>
+                        <Image src={Isekai} className="rounded-lg" />
+                      </TransformComponent>
+
+                      <div className="tools space-x-3 text-center py-3 text-sm">
+                        <button
+                          onClick={() => zoomIn()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom In
+                        </button>
+                        <button
+                          onClick={() => zoomOut()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom Out
+                        </button>
+                        <button
+                          onClick={() => resetTransform()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Reset
+                        </button>
+                      </div>
+                    </React.Fragment>
+                  )}
+                </TransformWrapper>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item relative float-left w-full">
+            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900 text-center">
+              <p className="p-5">
+                <div
+                  id="item1"
+                  className="text-xl font-bold mb-5 md:mt-0 mt-5 text-center"
+                >
+                  Map-Click Location Management System
+                </div>
+                Made with PHP Native and ReactJS, this app is a location
+                management system that uses Mapbox API to display the location.
+                This app also has a feature to detect weather. You can check the
+                website{" "}
+                <a
+                  href="https://mapbox.ginanjartg.me/"
+                  target="_blank"
+                  className="text-blue-500"
+                >
+                  here
+                </a>
+                .
+              </p>
+              <div className="relative mx-auto p-5">
+                <TransformWrapper initialScale={1}>
+                  {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                    <React.Fragment>
+                      <TransformComponent>
+                        <Image src={Location} className="rounded-lg" />
+                      </TransformComponent>
+
+                      <div className="tools space-x-3 text-center py-3 text-sm">
+                        <button
+                          onClick={() => zoomIn()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom In
+                        </button>
+                        <button
+                          onClick={() => zoomOut()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Zoom Out
+                        </button>
+                        <button
+                          onClick={() => resetTransform()}
+                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
+                        >
+                          Reset
+                        </button>
+                      </div>
+                    </React.Fragment>
+                  )}
+                </TransformWrapper>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item relative float-left w-full">
+            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900 text-center">
+              <p className="p-5">
+                <div
+                  id="item1"
+                  className="text-xl font-bold mb-5 md:mt-0 mt-5 text-center"
+                >
+                  Meatball Ingredients Management System
                 </div>
                 A CRUD system to save the data of meatball ingredients stock.
                 This app using CodeIgniter framework and also has been equipped
                 with a login system.
-              </p>
-              <div className="relative mx-auto p-5">
-                <TransformWrapper initialScale={1}>
-                  {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-                    <React.Fragment>
-                      <TransformComponent>
-                        <Image src={Bahan} className="rounded-lg" />
-                      </TransformComponent>
-
-                      <div className="tools space-x-3 text-center py-3 text-sm">
-                        <button
-                          onClick={() => zoomIn()}
-                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
-                        >
-                          Zoom In
-                        </button>
-                        <button
-                          onClick={() => zoomOut()}
-                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
-                        >
-                          Zoom Out
-                        </button>
-                        <button
-                          onClick={() => resetTransform()}
-                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
-                        >
-                          Reset
-                        </button>
-                      </div>
-                    </React.Fragment>
-                  )}
-                </TransformWrapper>
-              </div>
-            </div>
-          </div>
-          <div className="carousel-item relative float-left w-full">
-            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900 text-center">
-              <p className="p-5">
-                <div
-                  id="item1"
-                  className="text-xl font-bold mb-5 md:mt-0 mt-5 text-center"
-                >
-                  3D Landing Page of Meatball Restaurant
-                </div>
-                Made with Three.js, this landing page is a 3D restaurant website
-              </p>
-              <div className="relative mx-auto p-5">
-                <TransformWrapper initialScale={1}>
-                  {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-                    <React.Fragment>
-                      <TransformComponent>
-                        <Image src={Bahan} className="rounded-lg" />
-                      </TransformComponent>
-
-                      <div className="tools space-x-3 text-center py-3 text-sm">
-                        <button
-                          onClick={() => zoomIn()}
-                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
-                        >
-                          Zoom In
-                        </button>
-                        <button
-                          onClick={() => zoomOut()}
-                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
-                        >
-                          Zoom Out
-                        </button>
-                        <button
-                          onClick={() => resetTransform()}
-                          className="btn-primary bg-blue-500 hover:bg-blue-700 p-2 rounded"
-                        >
-                          Reset
-                        </button>
-                      </div>
-                    </React.Fragment>
-                  )}
-                </TransformWrapper>
-              </div>
-            </div>
-          </div>
-          <div className="carousel-item relative float-left w-full">
-            <div className="text-xs md:text-base block border-4 border-gray-500 rounded-xl bg-gray-900 text-center">
-              <p className="p-5">
-                <div
-                  id="item1"
-                  className="text-xl font-bold mb-5 md:mt-0 mt-5 text-center"
-                >
-                  Sistem Manajemen Lokasi By Maps
-                </div>
-                Made with React.js, this app is a location management system
-                that uses Mapbox API to display the location. This app also has
-                a feature to detect weather.
               </p>
               <div className="relative mx-auto p-5">
                 <TransformWrapper initialScale={1}>
