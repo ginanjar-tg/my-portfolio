64d699820bb35afa71fdd338d697cda9acce3c01
ginanjartg <ginanjar0822@gmail.com>
2023-09-05 14:04:41 +0700

    Update contents.js

--- pages/components/contents.js
+++ pages/components/contents.js
@@ -90,7 +90,7 @@ export default function Contents() {
                 </div>
                 <br />
                 <a
-                  href="https://ginanjartg.me/resume/cv.pdf"
+                  href="cv.pdf"
                   className="font-bold mt-10 inline-flex items-center p-3 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Download My CV
